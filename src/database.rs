85b2828da7bb1e61e221d5aa424ed4f1225900d9
Keith Gable <keith@ziggythehamster.sh>
2022-06-19 01:03:52 -0700

    Add an example/doctest to database::open

--- src/database.rs
+++ src/database.rs
@@ -6,7 +6,24 @@ use std::path::PathBuf;
 
 embed_migrations!();
 
-/// Opens the database and then initializes it if needed
+/// Opens the database and initializes it if needed
+///
+/// # Examples
+///
+/// ```
+/// use oplfs::database;
+/// use std::path::PathBuf;
+///
+/// let database_path = PathBuf::from("test.sqlite");
+/// # let database_path: PathBuf = [std::env::temp_dir(), PathBuf::from("test.sqlite")].iter().collect();
+/// # let database_path_to_delete = database_path.clone();
+/// let connection    = database::open(database_path)?;
+///
+/// // use Diesel on this connection
+///
+/// # std::fs::remove_file(database_path_to_delete.as_path());
+/// # Ok::<(), Box<dyn std::error::Error>>(())
+/// ```
 pub fn open(database_path: PathBuf) -> Result<SqliteConnection, Box<dyn Error>> {
     let connection = SqliteConnection::establish(&database_path.to_string_lossy())?;
     embedded_migrations::run(&connection)?;
