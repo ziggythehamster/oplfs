f0ba290f701773654591b6f61767310cf37cdf29
Keith Gable <keith@ziggythehamster.sh>
2022-06-18 21:55:52 -0700

    Define module

---
+++ src/disc/mod.rs
@@ -0,0 +1,12 @@
+mod disc;
+mod format;
+mod media;
+mod platform;
+mod video_mode;
+
+// Export our enums/struct
+pub use disc::Disc;
+pub use format::Format;
+pub use media::Media;
+pub use platform::Platform;
+pub use video_mode::VideoMode;
